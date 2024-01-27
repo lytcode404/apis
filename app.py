386f580405b369cecd297190bc7aa4d8bd513e41
Lyt Code <rdilshad3559@gmail.com>
2024-01-27 15:13:50 +0530

    new data

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-import pandas as pd
 import json
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 
-# Load data from the 'sample.json' file in the root directory
+# Load data from the 'day_stats.json' file in the root directory
 with open('day_stats.json', 'r') as json_file:
     sample_data = json.load(json_file)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST'])
 def get_data():
-    return jsonify(sample_data)
+    # Extracting JSON data from request body
+    data = request.get_json()
+
+    # Extracting 'name' from JSON data
+    name = data.get('name')
+
+    # Do something with the name if needed
+    # For now, just returning the name
+    return jsonify({'name': name})
 
 
 if __name__ == '__main__':
